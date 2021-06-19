7df460a7eb403c37fc6b990c3a7c68113c3d38e0
zzunny97 <zzunny97@gmail.com>
2021-06-19 09:20:48 +0000

    깡통 프로젝트 하나 업로드

---
+++ website/src/test/java/com/zzunny/website/WebsiteApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zzunny.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+    
+
+	@Test
+	void contextLoads() {
+        
+	}
+
+}
